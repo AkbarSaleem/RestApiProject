b8b84fc7ab7640967c4376b159d19c26a16bd509
Akbar <Akbar@Akbar-PC>
2018-01-09 09:44:07 +0530

    rest project

---
+++ RestApiProject/src/main/java/com/test/rest/RestApiProject/RestApp.java
@@ -0,0 +1,9 @@
+package com.test.rest.RestApiProject;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+/*@Path("rest")
+public class RestApp extends Application {
+
+}
+*/
---
+++ RestApiProject/src/main/java/com/test/rest/RestApiProject/RestClient.java
@@ -0,0 +1,37 @@
+package com.test.rest.RestApiProject;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+
+public class RestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String methodName="getEmployee";
+       
+      //  webTarget.register(Employee.class);
+		WebTarget webTarget=getWebTarget(methodName);
+		List<Employee> employeeList=  webTarget.request().get(new GenericType<List<Employee>>(){});
+     
+		for(Employee emp:employeeList){
+			System.out.println(emp.getEmpNo()+" "+emp.getEmpName());
+		}
+      
+	}
+	
+	
+	private static WebTarget getWebTarget(String methodName){
+		
+		Client client=ClientBuilder.newClient();
+	        WebTarget webTarget=client.target("http://localhost:8181/RestApiProject/webapi/employee/")
+	                      .path(methodName);
+	        System.out.println("This is for testing");
+	        return webTarget;
+	}
+
+}
